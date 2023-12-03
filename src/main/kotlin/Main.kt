f6807d8f0f15be8a0bbaac431a4778d3e9a1689f
Stenz123 <michael.stenz2006@gmail.com>
2023-12-03 08:00:01 +0100

    not working

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-import days.day1.Day1
+import days.day3.Day3
 
 fun main() {
-    val currentDay = Day1()
+    val currentDay = Day3()
 
     currentDay.solvePartOne()
     currentDay.solvePartTwo()
